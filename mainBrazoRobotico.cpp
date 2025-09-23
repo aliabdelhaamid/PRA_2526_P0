38e1eaf8d5aef29549f0aad42eec89ba62fd0e5d
AliAbdelhamid <aabdsal@upv.edu.es>
2025-09-23 20:28:22 +0200

    Acabado de verdad

--- mainBrazoRobotico.cpp
+++ mainBrazoRobotico.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 int main(){
 	double x,y,z;
+	
 	cout << "Dame coordenadas X,Y,Z: ";
-	cin << x << y << z;
+	cin >> x >> y >> z;
+	
 	BrazoRobotico r1(x,y,z);
 	r1.mover(3.2, 2.4, 6.7);
 	r1.coger();
+	
 	if(r1.getIsSujetando()){
 		cout << "Esta sujetandose" << endl;
 	}
